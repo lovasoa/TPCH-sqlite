e11a505898537b100e119dd7123231530c915a27
Ophir LOJKINE <pere.jobs@gmail.com>
2017-05-27 19:48:49 +0300

    fix csv format before importing it

--- create_db.sh
+++ create_db.sh
@@ -14,7 +14,6 @@ rm -f "$db"
 echo "Creating the database structure..." >&2
 sqlite3 "$db" < sqlite-ddl.sql
 
-errfile=$(mktemp)
 RET_CODE=0
 for table in $TABLES; do
 	echo "Importing table '$table'..." >&2
@@ -28,16 +27,15 @@ for table in $TABLES; do
 	(
 		echo ".mode csv";
 		echo ".separator |";
-		echo -n ".import $data_file ";
+		echo -n ".import /dev/stdin ";
 		echo $table | tr a-z A-Z;
-	) | sqlite3 "$db" 2>$errfile
+		sed -e 's/|$//' < "$data_file"
+	) | sqlite3 "$db"
 
 	if [ $? != 0 ]; then
 		echo "Import failed." >&2
-		cat $errfile >&2
 		RET_CODE=1
 	fi
 done
-rm $errfile
 
 exit $RET_CODE
